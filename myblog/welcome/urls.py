5e70086917ff6c64acd80cf7228c067ee787c361
mateuszlesko <mateusz.lesko@outlook.com>
2022-01-18 09:54:22 +0100

    own login module

--- myblog/welcome/urls.py
+++ myblog/welcome/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = "welcome"
 urlpatterns = [
-    path("",views.index),
+    path("",views.index,name="index"),
+    path("login",views.mylogin,name="login"),
+    path("profile",views.profile,name="profile"),
+    path("logout",views.logout,name="logout"),
+    path("changePassword",views.changePassword,name="changePassword"),
 ]
